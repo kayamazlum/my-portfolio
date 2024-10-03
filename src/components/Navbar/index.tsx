bb13a853bbfbca7343d7ccee3ae65c89e730c3ee
kayamazlum <mazlum09kaya@gmail.com>
2024-10-03 21:58:32 +0300

    hamburger menu yaptım

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,22 @@
+'use client';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import Section from '../Section';
+import { CgMenuRight } from 'react-icons/cg';
+import { GrClose } from 'react-icons/gr';
 
 const Navbar = () => {
+ const [hamburger, setHamburger] = useState(false);
+ console.log(hamburger);
+
+ const changeMenu = () => {
+  setHamburger(!hamburger);
+ };
+
  return (
-  <Section className="justify-start flex items-center h-[64px] border-b bg-customLight border-b-gray-300">
-   <div>
-    <ul className="flex gap-8 font-semibold text-[20px]">
+  <Section className="justify-start flex items-center h-[64px] border-b bg-customLight border-b-gray-300 ">
+   <div className=" w-full hidden justify-between md:flex">
+    <ul className="flex gap-8 font-semibold text-[20px] ">
      <li>
       <Link href={'/'}>Home</Link>
      </li>
@@ -21,6 +31,30 @@ const Navbar = () => {
      </li>
     </ul>
    </div>
+   <div className="flex md:hidden justify-end w-full">
+    <CgMenuRight size={40} onClick={() => changeMenu()} />
+   </div>
+   {hamburger && (
+    <div className="fixed h-screen w-screen left-0 top-0 bg-blue-400 z-50 justify-center flex flex-col">
+     <div className="justify-center absolute top-8 right-8">
+      <GrClose size={40} onClick={() => changeMenu()} />
+     </div>
+     <ul className="flex flex-col gap-8 font-semibold text-3xl items-center justify-center ">
+      <li>
+       <Link href={'/'}>Home</Link>
+      </li>
+      <li>
+       <Link href={'/'}>Portfolio</Link>
+      </li>
+      <li>
+       <Link href={'/'}>About</Link>
+      </li>
+      <li>
+       <Link href={'/'}>Contact</Link>
+      </li>
+     </ul>
+    </div>
+   )}
   </Section>
  );
 };
