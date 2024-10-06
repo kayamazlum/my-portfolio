ea22378c75a9039b4199420580a6f5f9a3fd165a
yusufocaliskan <yusufocaliskan@gmail.com>
2024-10-06 20:16:08 +0300

    hamburger hook yazildi.

---
+++ src/components/HamburgerMenu/index.tsx
@@ -0,0 +1,61 @@
+'use client';
+import scrollToSection from '@/helpers';
+import { GrClose } from 'react-icons/gr';
+import Navbar from '../Navbar';
+
+interface IHamburgerMenue {
+  changeMenu: () => void;
+}
+export const HamburgerMenu = ({ changeMenu }: IHamburgerMenue) => {
+  return (
+    <div className="fixed h-screen w-screen left-0 top-0 bg-customLight2 z-50 justify-center flex flex-col bg-gradient-to-r from-customLight2 to-customWhite">
+      <div className="justify-center absolute top-8 right-8">
+        <GrClose size={40} onClick={() => changeMenu()} />
+      </div>
+      <ul className="flex flex-col gap-8 font-semibold text-3xl items-center justify-center ">
+        <li className="">
+          <button
+            onClick={() => {
+              scrollToSection('top');
+              changeMenu();
+            }}
+          >
+            Home
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              scrollToSection('portfolio');
+              changeMenu();
+            }}
+          >
+            Portfolio
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              scrollToSection('about');
+              changeMenu();
+            }}
+          >
+            About
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              scrollToSection('contact');
+              changeMenu();
+            }}
+          >
+            Contact
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,19 +1,15 @@
 'use client';
-import React, { useState } from 'react';
-import Section from '../Section';
-import { CgMenuRight } from 'react-icons/cg';
-import { GrClose } from 'react-icons/gr';
 import scrollToSection from '@/helpers';
+import { useState } from 'react';
 import { MdLightMode, MdNightlight } from 'react-icons/md';
+import useHamburger from '../../hooks/useHambuger';
+import Section from '../Section';
 
 const Navbar = () => {
-  const [hamburger, setHamburger] = useState(false);
-  const changeMenu = () => {
-    setHamburger(!hamburger);
-  };
+  const { HamburgerMenuRender, HamburtMenuTrigger } = useHamburger();
 
+  //dark mde
   const [darkMode, setDarkMode] = useState(false);
-  console.log(darkMode);
   const changeModeHandler = () => {
     setDarkMode(!darkMode);
   };
@@ -42,57 +38,10 @@ const Navbar = () => {
         )}
       </div>
       <div className="flex md:hidden justify-end w-full">
-        <CgMenuRight size={40} onClick={() => changeMenu()} />
+        <HamburtMenuTrigger />
       </div>
-      {hamburger && (
-        <div className="fixed h-screen w-screen left-0 top-0 bg-customLight2 z-50 justify-center flex flex-col bg-gradient-to-r from-customLight2 to-customWhite">
-          <div className="justify-center absolute top-8 right-8">
-            <GrClose size={40} onClick={() => changeMenu()} />
-          </div>
-          <ul className="flex flex-col gap-8 font-semibold text-3xl items-center justify-center ">
-            <li className="">
-              <button
-                onClick={() => {
-                  scrollToSection('top');
-                  changeMenu();
-                }}
-              >
-                Home
-              </button>
-            </li>
-            <li>
-              <button
-                onClick={() => {
-                  scrollToSection('portfolio');
-                  changeMenu();
-                }}
-              >
-                Portfolio
-              </button>
-            </li>
-            <li>
-              <button
-                onClick={() => {
-                  scrollToSection('about');
-                  changeMenu();
-                }}
-              >
-                About
-              </button>
-            </li>
-            <li>
-              <button
-                onClick={() => {
-                  scrollToSection('contact');
-                  changeMenu();
-                }}
-              >
-                Contact
-              </button>
-            </li>
-          </ul>
-        </div>
-      )}
+
+      <HamburgerMenuRender />
     </Section>
   );
 };
