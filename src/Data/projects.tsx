926c6c5363a5e3eb896d54522bdb0b791efd420e
kayamazlum <mazlum09kaya@gmail.com>
2024-10-21 17:52:01 +0300

    veriler arttırıldı

--- src/Data/projects.tsx
+++ src/Data/projects.tsx
@@ -2,7 +2,8 @@ const ProjectsData = [
   {
     id: '1',
     title: 'Coming Soon',
-    content: 'Designed some empty state screens in kawaii style for the popular sheepify app',
+    content:
+      'Designed some empty state screens in kawaii style for the popular sheepify app. A colorful dashboard to manage your fictional projects and team, all in a single place. redesigned the popular language.. A colorful dashboard to manage your fictional projects and team, all in a single place. redesigned the popular language.. A colorful dashboard to manage your fictional projects and team, all in a single place. redesigned the popular language.',
     imageUrl: 'https://gratisography.com/wp-content/uploads/2024/03/gratisography-funflower-1170x780.jpg',
     tec: ['ReactJS', 'TailwindCSS', 'TypeScript'],
   },
@@ -10,7 +11,7 @@ const ProjectsData = [
     id: '2',
     title: 'Coming Soon',
     content:
-      'A colorful dashboard to manage your fictional projects and team, all in a single place. redesigned the popular language.',
+      'A colorful dashboard to manage your fictional projects and team, all in a single place. redesigned the popular language. Designed some empty state screens in kawaii style for the popular sheepify app. A colorful dashboard to manage your fictional projects and team, all in a single place. redesigned the popular language.',
     imageUrl:
       'https://images.unsplash.com/photo-1724726561649-c94ea9057a20?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     tec: ['NextJS', 'TailwindCSS', 'MongoDB', 'TypeScript'],
@@ -18,7 +19,8 @@ const ProjectsData = [
   {
     id: '3',
     title: 'Coming Soon',
-    content: 'I redesigned the popular language learning app memrise in kawaii style to make it more cute.',
+    content:
+      'I redesigned the popular language learning app memrise in kawaii style to make it more cute. Designed some empty state screens in kawaii style for the popular sheepify app. A colorful dashboard to manage your fictional projects and team, all in a single place. redesigned the popular language.Designed some empty state screens in kawaii style for the popular sheepify app. A colorful dashboard to manage your fictional projects and team, all in a single place. redesigned the popular language. I redesigned the popular language learning app memrise in kawaii style to make it more cute. I redesigned the popular language learning app memrise in kawaii style to make it more cute.',
     imageUrl:
       'https://images.unsplash.com/photo-1719937206158-cad5e6775044?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     tec: [
